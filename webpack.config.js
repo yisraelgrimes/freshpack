99af74e2da7bc52a756ab598a98e58d9a3a5c55b
Yisrael Grimes <yisraelgrimes@gmail.com>
2019-10-09 13:48:34 -0500

    updated packages and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const path = require(`path`)
 
 module.exports = {
-	entry: `./src/index.js`,
+	entry: [`babel-polyfill`, `./src/scripts/index.js`],
+	// If multiple pages with different js files
+	// entry: {
+	// 	index: [`babel-polyfill`, `./src/scripts/index.js`],
+	// 	about: [`babel-polyfill`, `./src/scripts/about.js`],
+	// },
 	output: {
 		path: path.resolve(__dirname, `public/scripts`),
-		filename: `main.js`
+		filename: `main.js`,
+		// If multiple pages with different js files
+		// filename: `[name]-bundle.js`, // replaces [name] with "entry" object-key.
 	},
 	module: {
 		rules: [{
@@ -20,14 +27,19 @@ module.exports = {
 				loader: `babel-loader`,
 				options: {
 					presets: [`@babel/env`],
+					plugins: [`transform-object-rest-spread`],
 				},
 			},
 		}],
 	},
 	devServer: {
+		// Path or array of paths for files that aren't generated dynamically
 		contentBase: path.resolve(__dirname, `public`),
 		publicPath: `/scripts/`,
 		port: 9000,
+		stats: `errors-only`, // Display only errors to reduce the amount of output.
+		open: true, // Open the page in browser
+		overlay: true, // Overlay errors onto the webpage
 	},
 	devtool: `source-map`,
 }
