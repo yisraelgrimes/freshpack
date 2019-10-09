954cb8bf95a65c4bb5e8bd721fe7317b0d2d3a48
Yisrael Grimes <yisraelgrimes@gmail.com>
2019-10-09 13:48:10 -0500

    updated lint rules

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+	extends: "@workwithizzi/eslint-config",
+	rules: {
+		"quotes": ["warn", "backtick"],
+		semi: ["warn", "never"],
+		"comma-dangle": ["warn", {
+			"arrays": "always-multiline",
+			"objects": "always-multiline",
+			"imports": "always-multiline",
+			"exports": "always-multiline",
+			"functions": "never"
+	}]
+	}
+}
